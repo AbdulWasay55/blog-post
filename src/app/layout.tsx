83a6d2244225b62ac4761c1c74464fb1a0dbb9e6
AbdulWasay55 <162962561+AbdulWasay55@users.noreply.github.com>
2025-01-03 15:23:05 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
+import Nav from "./components/nav";
+import Footer from "./components/Footer/Footer";
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+          <Nav/>
         {children}
+        <Footer/>
+
       </body>
     </html>
   );
